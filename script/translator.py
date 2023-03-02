f5a8b9f9680597101b5e32d4e44c913797a311e4
ivhu <ivhu@foxmail.com>
2023-03-02 11:14:42 +0800

    perf: 只保留谷歌翻译

--- script/translator.py
+++ script/translator.py
@@ -568,14 +568,14 @@ class SdcvShell(BaseTranslator):
 
 
 ENGINES = {
-    "baicizhan": BaicizhanTranslator,
-    "bing": BingDict,
-    "haici": HaiciDict,
+    # "baicizhan": BaicizhanTranslator,
+    # "bing": BingDict,
+    # "haici": HaiciDict,
     "google": GoogleTranslator,
-    "iciba": ICibaTranslator,
-    "sdcv": SdcvShell,
-    "trans": TranslateShell,
-    "youdao": YoudaoTranslator,
+    # "iciba": ICibaTranslator,
+    # "sdcv": SdcvShell,
+    # "trans": TranslateShell,
+    # "youdao": YoudaoTranslator,
 }
 
 
@@ -632,46 +632,45 @@ def main():
 
 
 if __name__ == "__main__":
+    # def test0():
+    #     t = BaseTranslator("test_proxy")
+    #     t.set_proxy("http://localhost:8087")
+    #     t.test_request("https://www.google.com")
+    #
+    # def test1():
+    #     t = BaicizhanTranslator()
+    #     r = t.translate("", "zh", "naive")
+    #     print(r)
+    #
+    # def test2():
+    #     t = BingDict()
+    #     r = t.translate("", "", "naive")
+    #     print(r)
+    #
+    # def test3():
+    #     gt = GoogleTranslator()
+    #     r = gt.translate("auto", "zh", "filencodings")
+    #     print(r)
+    #
+    # def test4():
+    #     t = HaiciDict()
+    #     r = t.translate("", "zh", "naive")
+    #     print(r)
+    #
+    # def test5():
+    #     t = ICibaTranslator()
+    #     r = t.translate("", "", "naive")
+    #     print(r)
+    #
+    # def test6():
+    #     t = TranslateShell()
+    #     r = t.translate("auto", "zh", "naive")
+    #     print(r)
 
-    def test0():
-        t = BaseTranslator("test_proxy")
-        t.set_proxy("http://localhost:8087")
-        t.test_request("https://www.google.com")
-
-    def test1():
-        t = BaicizhanTranslator()
-        r = t.translate("", "zh", "naive")
-        print(r)
-
-    def test2():
-        t = BingDict()
-        r = t.translate("", "", "naive")
-        print(r)
-
-    def test3():
-        gt = GoogleTranslator()
-        r = gt.translate("auto", "zh", "filencodings")
-        print(r)
-
-    def test4():
-        t = HaiciDict()
-        r = t.translate("", "zh", "naive")
-        print(r)
-
-    def test5():
-        t = ICibaTranslator()
-        r = t.translate("", "", "naive")
-        print(r)
-
-    def test6():
-        t = TranslateShell()
-        r = t.translate("auto", "zh", "naive")
-        print(r)
-
-    def test7():
-        t = YoudaoTranslator()
-        r = t.translate("auto", "zh", "naive")
-        print(r)
+    # def test7():
+    #     t = YoudaoTranslator()
+    #     r = t.translate("auto", "zh", "naive")
+    #     print(r)
 
     # test3()
     main()
